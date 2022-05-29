742bd9e80fad7b596856235548c41a8ba6fbb4e9
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-29 17:56:52 -0500

    implement Text Field

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,24 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+    @IBAction func searchPressed(_ sender: UIButton) {
+        processSearch()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        processSearch()
         return true
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+    func processSearch() {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.placeholder = "Search"
+        searchTextField.text = ""
     }
 }
 
