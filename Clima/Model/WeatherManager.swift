658ced8686deb8eba11b7e874e539acec8185a8a
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-30 17:18:51 -0500

    Add WeatherManager

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Xinyi Zhu on 5/30/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=3f7ab559ef8a26ebc364f1f10a31e901&units=metric&q=london"
+}
