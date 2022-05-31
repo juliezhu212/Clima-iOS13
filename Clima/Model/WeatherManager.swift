5f678b93671708088149c6220d664b47424cb369
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-31 17:31:08 -0500

    create WeatherModel

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -39,23 +39,18 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            
             let id = decodedData.weather[0].id
-            print(getConditionName(weatherId: id))
+            let name = decodedData.name
+            let temp = decodedData.main.temp
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weather.temperatureString)
+            print(weather.conditionName)
         }  catch {
             print(error)
         }
     }
     
-    func getConditionName(weatherId: Int) -> String {
-        switch weatherId {
-        case 200...299: return "cloud.bolt"
-        case 300...399: return "cloud.bolt.rain"
-        case 500...599: return "cloud.rain"
-        case 600...699: return "snowflake"
-        case 700...799: return "tornado"
-        case 800: return "sun.max"
-        case 801...809: return "cloud"
-        default: return "unknown"
-        }
-    }
+    
 }
