08391ceed84fbb842890d39a0f7f3ec2709c5f4c
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-30 21:33:55 -0500

    finish parseJSON

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -23,22 +23,29 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
             
             // 4. Start the task
             task.resume()
         }
     }
     
-    func handle(data:Data?, response:URLResponse?, error:Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString!)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        }  catch {
+            print(error)
         }
     }
 }
